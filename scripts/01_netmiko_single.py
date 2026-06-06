15af05681b1b84450d104b142e4064bf50a8a96f
manishkulkarni13 <manishkulkarni13@gmail.com>
2026-06-06 12:11:02 -0700

    feat:netmiko single device connect and push

---
+++ scripts/01_netmiko_single.py
@@ -0,0 +1,31 @@
+from netmiko import ConnectHandler
+from netmiko.exceptions import NetmikoTimeoutException, NetmikoAuthenticationException
+
+device = {
+    "device_type": "arista_eos",
+    "host": "172.20.20.11",
+    "username": "admin",
+    "password": "admin",
+    "secret":"admin"
+}
+
+try:
+    with ConnectHandler(**device) as conn:
+        conn.enable()
+        print(f"Connected to {conn.find_prompt()}")
+        output = conn.send_command("show version")
+        print(output)
+        config = [
+            "interface Loopback100",
+            "description MANAGED_BY_AUTOMATION",
+            "ip address 10.100.0.1 255.255.255.255",
+        ]
+        result = conn.send_config_set(config)
+        print(result)
+
+        verify = conn.send_command("show interface Loopback100")
+
+except NetmikoTimeoutException:
+    print("ERROR: Device unreachable")
+except NetmikoAuthenticationException:
+    print("Error: Bad Credentials")
